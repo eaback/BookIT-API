b3e6e7061b3fdf44aaa4ba67d1b48bcb99e5d065
tobbesoren <113234609+tobbesoren@users.noreply.github.com>
2023-11-13 18:08:30 +0100

    startedWorkingOnBooking

---
+++ functions/bookRoom/index.js
@@ -0,0 +1,37 @@
+const AWS = require("aws-sdk");
+const { sendResponse } = require("../../responses");
+const db = new AWS.DynamoDB.DocumentClient();
+const uuid = require("uuid");
+
+async function bookRoom(roomId) {
+  const bookingId = uuid.v4();
+  const newBooking = {
+    id: `${bookingId}`,
+  };
+  try {
+    await db
+      .update({
+        TableName: "rooms-db",
+        Key: {
+          id: roomId,
+        },
+        UpdateExpression:
+          "set Bookings = list_append(if_not_exists(Bookings, :empty_list), :newBooking)",
+        ExpressionAttributeValues: {
+          ":newBooking": [newBooking],
+          ":empty_list": [],
+        },
+      })
+      .promise();
+    return { success: true, booking: newBooking };
+  } catch (error) {
+    return { success: false, error: error };
+  }
+}
+
+exports.handler = async (event, context) => {
+  const roomId = event.pathParameters.roomId;
+  const response = await bookRoom(roomId);
+
+  return sendResponse(response.success ? 200 : response.error.errorCode, response);
+};
