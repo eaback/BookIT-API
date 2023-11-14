badeaf6a9d1f54431d4ccf69bc1fa77717da9d86
tobbesoren <113234609+tobbesoren@users.noreply.github.com>
2023-11-14 14:51:11 +0100

    bookRoomWithLimitedErrorHandling

--- functions/bookRoom/index.js
+++ functions/bookRoom/index.js
@@ -3,13 +3,62 @@ const { sendResponse } = require("../../responses");
 const db = new AWS.DynamoDB.DocumentClient();
 const uuid = require("uuid");
 
-async function bookRoom(roomId) {
-  const bookingId = uuid.v4();
-  const newBooking = {
-    //Här skall vi skapa ett nytt booking-objekt
-    id: `${bookingId}`,
-  };
+exports.handler = async (event, context) => {
+  const roomId = event.pathParameters.roomId;
+  const bookingInfo = JSON.parse(event.body);
 
+  const roomResponse = await getRoom(roomId);
+
+  if (!roomResponse.success) {
+    //First, we check for the room
+    // If it doesn't exist, return with response from getRoom
+    return sendResponse(400, roomResponse);
+  }
+  // Success! Let's extract the room for easier use.
+  const room = roomResponse.room;
+  // We should have a new Booking object, too:
+  const newBooking = createBooking(roomId, room, bookingInfo);
+
+  // Now we have the room and Booking, so we proceed to book the room:
+  const bookingResponse = await bookRoom(roomId, newBooking);
+
+  // Lets return whether the operation was successful or not
+  return sendResponse(bookingResponse.success ? 200 : 400, {
+    response: bookingResponse,
+    room: roomResponse,
+    bookingInfo: bookingInfo,
+  });
+};
+
+function createBooking(roomId, room, bookingInfo) {
+  const bookingId = uuid.v4();
+  const totalNigths = getTotalNigths(
+    bookingInfo.CheckInDate,
+    bookingInfo.CheckOutDate
+  );
+  const pricePerNight = room.PricePerNight;
+  const totalPrice = totalNigths * pricePerNight;
+
+  let newBooking = bookingInfo;
+
+  newBooking["BookingId"] = `${bookingId}`;
+  newBooking["TotalNigths"] = totalNigths;
+  newBooking["TotalPrice"] = totalPrice;
+  newBooking["RoomId"] = roomId;
+  newBooking["Status"] = "Confirmed";
+
+  return newBooking;
+}
+
+function getTotalNigths(checkInDate, checkOutDate) {
+  const startDate = new Date(checkInDate);
+  const endDate = new Date(checkOutDate);
+  const totalNigths =
+    (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24);
+  return totalNigths;
+}
+
+async function bookRoom(roomId, newBooking) {
   try {
     await db
       .update({
@@ -58,19 +107,3 @@ async function getRoom(roomId) {
   }
   return response;
 }
-
-exports.handler = async (event, context) => {
-  const roomId = event.pathParameters.roomId;
-  const room = await getRoom(roomId);
-
-  if (!room.success) {
-    return sendResponse(400, room);
-  }
-
-  const response = await bookRoom(roomId);
-
-  return sendResponse(response.success ? 200 : 400, {
-    response: response,
-    room: room,
-  });
-};
