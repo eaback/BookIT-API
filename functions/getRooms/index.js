9f50f03d2ebac82279c8525aae3775ba1437e2ed
EaBack <113344913+EaBack@users.noreply.github.com>
2023-11-15 11:53:53 +0100

    error handling GETrooms

--- functions/getRooms/index.js
+++ functions/getRooms/index.js
@@ -3,11 +3,21 @@ const { sendResponse } = require("../../responses");
 const db = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event, context) => {
+
+  try{
   const { Items } = await db
     .scan({
       TableName: "rooms-db",
     })
     .promise();
 
-  return sendResponse(200, { success: true, rooms: Items });
+    if (!Items || !Items.length === 0) {
+      return sendResponse(404, {success: false, message: " No rooms found"})
+    }
+
+    return sendResponse(200, { success: true, rooms: Items });
+  } catch (error){
+      console.error("Error:", error);
+      return sendResponse(500, {success: false, message: "Internal Server Error"});
+    }
 };
