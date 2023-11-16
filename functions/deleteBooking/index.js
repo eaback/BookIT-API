1cee392663e6685ea7a517a3078ab4408ac71b4a
Philip Miranda <philipmir@hotmail.com>
2023-11-16 18:43:55 +0100

    update 2 not working, my database is a mess

--- functions/deleteBooking/index.js
+++ functions/deleteBooking/index.js
@@ -3,7 +3,7 @@ const AWS = require('aws-sdk');
 const db = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event, context) => {
-    const { roomId, bookingId } = event.pathParameters;
+    const { roomId } = event.pathParameters;
 
     try {
         // Fetch the room details
@@ -17,16 +17,10 @@ exports.handler = async (event, context) => {
             return sendResponse(404, { success: false, message: 'Room not found' });
         }
 
-        // Check if the booking exists
-        const bookingIndex = roomDetails.Item.booking.findIndex(booking => booking.id === bookingId);
-        if (bookingIndex === -1) {
-            return sendResponse(404, { success: false, message: 'Booking not found' });
-        }
+        // Remove the 'Bookings' array from the room details
+        delete roomDetails.Item.Bookings;
 
-        // Remove the booking from the array
-        roomDetails.Item.booking.splice(bookingIndex, 1);
-
-        // Update the room without the deleted booking
+        // Update the room without the 'Bookings' array
         await db.put({
             TableName: 'rooms-db',
             Item: roomDetails.Item
@@ -34,6 +28,7 @@ exports.handler = async (event, context) => {
 
         return sendResponse(200, { success: true });
     } catch (error) {
-        return sendResponse(500, { success: false, message: 'Could not delete booking' });
+        return sendResponse(500, { success: false, message: 'Could not remove bookings from room' });
     }
 };
+
